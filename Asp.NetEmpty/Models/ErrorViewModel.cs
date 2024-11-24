5f21d2c725a0894ad0dbe1fdf5dd3295a00c0c5d
Serhatuzn <seerhatuzun@gmail.com>
2024-11-25 00:19:16 +0300

    MVC Projects

---
+++ Asp.NetEmpty/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.NetEmpty.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Asp.NetEmpty/Program.cs
@@ -0,0 +1,46 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
+
+
+/*
+    Açıklamalar:
+
+    - Controller: Uygulamanın kullanıcı isteklerini işleyen kısmıdır. Örnek: HomeController.
+    - Action: Controller içinde bulunan, belirli bir görevi gerçekleştiren metodlardır.
+    - Model: Uygulamanın veri yapısını temsil eder. Örnek: Product modeli.
+    - View: Kullanıcıya sunulan HTML/CSS gibi çıktıları temsil eder.
+    - Razor: C# kodlarını HTML içinde yazmamıza olanak tanıyan bir şablon motorudur.
+    - RazorView: Razor şablonlarını işleyen yapı.
+    - wwwroot: Statik dosyaların (CSS, JS, görüntüler vb.) bulunduğu klasördür.
+    
+    - builder.Build(): Uygulama yapılandırmasını tamamlar ve bir "WebApplication" örneği oluşturur.
+    - app.Run(): Uygulamayı başlatır ve gelen HTTP isteklerini dinler.
+    
+*/
